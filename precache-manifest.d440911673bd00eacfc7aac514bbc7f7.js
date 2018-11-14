c72f556b241251935eecdc0be78815d190866b67
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2018-11-14 21:56:19 +0530

    Updates

---
+++ precache-manifest.d440911673bd00eacfc7aac514bbc7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "528e988f0f09420c6ed1",
+    "url": "/static/js/main.528e988f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "528e988f0f09420c6ed1",
+    "url": "/static/css/main.1684a19e.chunk.css"
+  },
+  {
+    "revision": "542010411ca1288e067626be068093b7",
+    "url": "/index.html"
+  }
+];
